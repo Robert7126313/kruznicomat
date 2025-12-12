7120e5a4fa801dc70075ab96a6d5bbcbc601aa8a
Robert712 <rposkocil@gmail.com>
2025-12-12 15:30:59 +0100

    Přidána elipsa

---
+++ src/Ellipse.java
@@ -0,0 +1,10 @@
+public class Ellipse {
+    final int x, y, width, height;
+
+    public Ellipse(int x, int y, int width, int height){
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+    }
+}
